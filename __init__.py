2f70e8a3a35ad7cab0ec357886e6ea88443821a5
NikChao <nik.chaourov@gmail.com>
2016-07-07 08:24:54 -0400

    added 404 handler and a skeleton for blog section

--- __init__.py
+++ __init__.py
@@ -8,5 +8,16 @@ def homepage():
     except Exception, e:
 	return str(e)
 
+@app.route('/blogs')
+def blog_hub():
+    try:
+	return render_template("blog_hub.html")
+    except Exception, e:
+	return str(e)
+    
+@app.errorhandler(404)
+def page_not_found(e):
+    return "Sorry mate, no page here, will make this error message prettier next time"
+
 if __name__ == "__main__":
     app.run
